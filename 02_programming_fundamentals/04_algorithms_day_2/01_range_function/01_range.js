24e51402cfd9d8ab9a03b61a8fb53b609647b48d
cdedkt <christophe.delattre@decathlon.com>
2018-04-06 16:24:32 +0200

    premiere fonction

--- 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
+++ 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
@@ -4,8 +4,18 @@
 // This function should work in both ascending or descending order.
 
 // Complete this function.
-function range() {
-
+function range(n1, n2) {
+  let res = [];
+  if (n1<n2) {
+    for(let i=n1; i<=n2; i++){
+      res.push(i);
+    }
+  } else {
+    for(let i=n1; i>=n2; i--){
+      res.push(i);
+    }
+  }
+  return res;
 }
 
 // Do not remove last lines, it is for tests
