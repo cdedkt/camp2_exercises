81d833c82c8f6965021fb12c32b07e2f12fd69bf
cdedkt <christophe.delattre@decathlon.com>
2018-04-06 14:05:50 +0200

    les boucles

--- 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/04_filtering.js
+++ 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/04_filtering.js
@@ -2,3 +2,12 @@
 //
 // -  Create an array called `litteralDigits` from `zero` to `nine` where each array entry is a spelled-out number;
 // -  Using `length`, write on `stdout` each odd values of the table.
+
+
+const litteralDigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+for (let i=0; i<litteralDigits.length; i++) {
+  if (i % 2 === 1) {
+    console.log(litteralDigits[i]);
+  }
+}
