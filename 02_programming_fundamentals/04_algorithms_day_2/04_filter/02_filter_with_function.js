c6af3b6c7c2b9b806e6d6b121e5f25e3323b8423
cdedkt <christophe.delattre@decathlon.com>
2018-04-06 17:47:54 +0200

    enfin

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
@@ -1,7 +1,16 @@
 // filter takes an array of integer and a function of filtering
 // such as filter([1, 2, 3, 4, 5], pickEvenNumbers) returns [2, 4]
+
+
+
 function filter(array, fn) {
-  // Your code here
+  let res = [];
+  for (let i=0; i<array.length; i++) {
+    if (fn(array[i])) {
+      res.push(array[i]);
+    }
+  }
+  return res;
 }
 
 // do not remove this line, it is for tests
