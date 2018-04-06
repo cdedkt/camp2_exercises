1e173464624d6ceb9b96a07b5432c0872e6e9267
cdedkt <christophe.delattre@decathlon.com>
2018-04-06 18:24:18 +0200

    mon premier reducer

--- 02_programming_fundamentals/04_algorithms_day_2/07_join_array/01_index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/07_join_array/01_index.js
@@ -5,6 +5,19 @@
 // eg: join(["zero", "one", "two"], "-") => "zero - one - two"
 
 
+
+function joinArray(array, separator) {
+  function monReducer(accumulator, currentValue) {
+    if (accumulator === "") {
+      return currentValue;
+    } else {
+      return accumulator + separator + currentValue;
+    }
+  }
+
+  return array.reduce(monReducer, "");
+}
+
 // ⚠ Do not remove me ! It's for tests
 // eslint-disable-next-line
 module.exports = joinArray;
