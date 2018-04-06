d248094d2457bfaa945597843ae4281082a58feb
cdedkt <christophe.delattre@decathlon.com>
2018-04-06 14:56:11 +0200

    pattern ok

--- 02_programming_fundamentals/03_algorithms_day_1/06_pattern/complete_the_pattern.js
+++ 02_programming_fundamentals/03_algorithms_day_1/06_pattern/complete_the_pattern.js
@@ -1,3 +1,14 @@
 // Modify this value to test with other values
 const numberOfLine = 5;
 // Your code here ⬇
+let chaine = "";
+
+for (let i=0; i<numberOfLine; i++) {
+  for (let j=numberOfLine; j>i; j--) {
+    chaine = chaine + j;
+  }
+  if (i<numberOfLine-1) {
+    chaine = chaine + "\n";
+  }
+}
+console.log(chaine);
