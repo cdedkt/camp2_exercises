b717a40be2a11190c0fe54f98f282b70975aa57f
cdedkt <christophe.delattre@decathlon.com>
2018-04-09 17:35:27 +0200

    color is done

--- 02_programming_fundamentals/05_callbacks/07_colorful_numbers/colorful.js
+++ 02_programming_fundamentals/05_callbacks/07_colorful_numbers/colorful.js
@@ -10,7 +10,62 @@
 
 // Insert your code here ⇩
 
+function ajouteUneLigne(ligne, longueur, allGroupe) {
+  let pos = 0;
+  while (pos <= ligne.length-longueur) {
+    const partie = ligne.slice(pos, pos+longueur);
+    //console.log("partie=" + partie);
+    allGroupe.push(partie);
+    pos = pos + 1;
+  }
+}
 
+function calculeUnProduit(element) {
+  const elementStr = element.toString();
+  let total = 1 * elementStr[0];
+  for (let i=1; i<elementStr.length; i++) {
+    total = total * elementStr[i];
+  }
+  console.log("element=" + element + ", produit=" + total);
+  return total;
+}
+
+function calculeTousLesProduits(allGroupe) {
+  return allGroupe.map(calculeUnProduit);
+}
+
+function existeValeurEnDouble(lesProduits) {
+  lesProduits.sort();
+  console.log(lesProduits);
+  for (let i=0; i<lesProduits.length-1; i++) {
+    if (lesProduits[i]===lesProduits[i+1]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isColorful(number) {
+  let allGroupe = [];
+
+  const numberStr = number.toString();
+  const nbBoucle = numberStr.length;
+  console.log("numberStr=" + numberStr + ", nbBoucle=" + nbBoucle);
+
+  for (let i=1; i<=nbBoucle; i++) {
+    ajouteUneLigne(numberStr, i, allGroupe);
+  }
+
+  let lesProduits = calculeTousLesProduits(allGroupe);
+
+  const result = !existeValeurEnDouble(lesProduits);
+  console.log("result=" + result);
+  return result;
+
+}
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
 module.exports = isColorful;
+
+
+console.log(isColorful(3245));
