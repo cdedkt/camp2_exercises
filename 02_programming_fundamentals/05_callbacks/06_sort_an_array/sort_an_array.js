fa51e1ab50fddd4078779bff07351affe9db5c40
cdedkt <christophe.delattre@decathlon.com>
2018-04-09 15:24:12 +0200

    sort

--- 02_programming_fundamentals/05_callbacks/06_sort_an_array/sort_an_array.js
+++ 02_programming_fundamentals/05_callbacks/06_sort_an_array/sort_an_array.js
@@ -7,8 +7,32 @@
 // Note: You should not use Array.sort()
 
 function sort(unsortedArray) {
-  // Your code here
+  let sortedArray = [];
+
+  while (unsortedArray.length>0) {
+    console.log(unsortedArray);
+
+    let indiceElementMin = 0;
+    let lastMinValue = unsortedArray[0];
+
+    for (let i=0; i<unsortedArray.length; i++) {
+      console.log(unsortedArray[i]);
+      if (unsortedArray[i] < lastMinValue) {
+        lastMinValue = unsortedArray[i];
+        indiceElementMin = i;
+      }
+    }
+    console.log(indiceElementMin);
+    sortedArray.push(unsortedArray[indiceElementMin]);
+    unsortedArray.splice(indiceElementMin,1);
+
+    console.log(unsortedArray);
+  }
+
+  return sortedArray;
 }
+
+console.log(sort([24, 7, 9, -10, 72, -8]));
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
