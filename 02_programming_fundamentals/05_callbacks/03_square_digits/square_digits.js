2a9e53a96ff6c4a4dccb2870c39b5e57ac70dc92
cdedkt <christophe.delattre@decathlon.com>
2018-04-09 12:28:44 +0200

    go

--- 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
+++ 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
@@ -8,9 +8,18 @@
 // Note: The function accepts an integer and returns an integer
 
 function squareDigits(number) {
-  // Your code here
+  const numberStr = number.toString();
+  let res = "";
+  for (let i=0; i< numberStr.length; i++) {
+    //console.log(numberStr[i]);
+    res = res + numberStr[i]*numberStr[i];
+    //console.log(res);
+  }
+  return Math.floor(res);
 }
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
 module.exports = squareDigits;
+
+//squareDigits(9129);
