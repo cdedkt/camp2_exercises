aad8edc352ef93e05740776c62fc2118983c8cc5
cdedkt <christophe.delattre@decathlon.com>
2018-04-11 17:24:11 +0200

    finder 1 OK

--- 02_programming_fundamentals/07_file_management/02_finder_1/finder.js
+++ 02_programming_fundamentals/07_file_management/02_finder_1/finder.js
@@ -29,29 +29,43 @@ const reader = readline.createInterface({
 });
 
 function displayFolderDetail(folder) {
-  console.log("folder : ", folder);
+  //console.log("folder : ", folder);
   fs.readdir(folder, (err, elements) => {
     if (err) {
       console.log(err);
     } else {
+      let elementIsDirectory = [];
+      console.log("0. ../");
       elements.forEach((element, index) => {
         const elementWithPath = path.resolve(folder, element);
-        fs.stat(elementWithPath, (err, stat) => {
-          if (stat && stat.isDirectory()) {
-            console.log((index+1) + ". " + element + "/");
-          } else {
-            console.log((index+1) + ". " + element);
-          }
-        }); // fs.stat
+        const stat = fs.statSync(elementWithPath);
+        if (stat && stat.isDirectory()) {
+          console.log((index+1) + ". " + element + "/");
+          elementIsDirectory[index] = true;
+        } else {
+          console.log((index+1) + ". " + element);
+          elementIsDirectory[index] = false;
+        }
+
       }); // forEach
       reader.question("Choose a number > ", currentAction => {
-          console.log(currentAction);
+          //console.log(currentAction);
           if (currentAction === "q") {
             console.log("AU REVOIR");
             reader.close();
+          } else if (currentAction === "0") {
+            const newFolder = path.resolve(folder, "..");
+            displayFolderDetail(newFolder);
           } else {
-            //traitement
-            displayFolderDetail(folder);
+            //traitement folder or file
+            if (elementIsDirectory[currentAction-1]) {
+              const newFolder = path.resolve(folder, elements[currentAction-1]);
+              displayFolderDetail(newFolder);
+            } else {
+              const currentFile = path.resolve(folder, elements[currentAction-1]);
+              console.log(fs.readFileSync(currentFile, "utf8"));
+              displayFolderDetail(folder);
+            }
           }
       }); //reader.question
     }
