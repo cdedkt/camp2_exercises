4bbbad9a86c4c7635112068c1a1e85b975426431
cdedkt <christophe.delattre@decathlon.com>
2018-04-11 14:57:33 +0200

    touch me if you can

--- 02_programming_fundamentals/07_file_management/01_basic_fs/05_touch.js
+++ 02_programming_fundamentals/07_file_management/01_basic_fs/05_touch.js
@@ -1,5 +1,34 @@
 // Using the file creation commands, create a touch function that mimics the behavior of the Unix command.
+const fs = require("fs");
+const path = require("path");
+
+function touch(filename) {
+  fs.access(filename, fs.constants.F_OK, (err) => {
+    if (!err) {
+      console.log("filename already exist");
+      const d = new Date();
+      fs.utimes(filename, d, d, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Date has been changed");
+        }
+      }); //fs.utimes
+    } else {
+      fs.writeFile(filename, "", (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("The file has been created!");
+        }
+      }); //fs.writeFile
+    }
+  }); //fs.access
 
 
+
+}
+
+touch("temp.txt");
 
 module.exports = touch;
