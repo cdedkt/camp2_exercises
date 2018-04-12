61d0122027e6e81f6e26ed22d9d46cde1fcedc1f
cdedkt <christophe.delattre@decathlon.com>
2018-04-12 10:47:10 +0200

    ca va vite

--- 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
+++ 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
@@ -1,3 +1,27 @@
 // See Sparta courses for the exercise summary
 
+const car = {
+  distance: 0,
+  speed: 0,
+
+  start: function() {
+    this.distance = 0;
+    return this;
+  },
+  showDistance: function() {
+    console.log(this.distance + " Km");
+    //return `Hello! I'm ${this.fullname()} and I'm ${this.age}`;
+  },
+  changeSpeed: function(speed) {
+    this.speed = speed;
+    return this;
+  },
+  drive: function(minutes) {
+    this.distance = this.distance + this.speed * minutes / 60;
+    return this;
+  }
+};
+
+car.start().changeSpeed(130).drive(42).showDistance();
+
 module.exports = car;
