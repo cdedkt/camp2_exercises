80897f75f12cf5e05246ea86d5e712f9ce77278c
cdedkt <christophe.delattre@decathlon.com>
2018-04-12 15:15:25 +0200

    json is perfect to save datas

--- 02_programming_fundamentals/07_file_management/04_number_game/number_game.js
+++ 02_programming_fundamentals/07_file_management/04_number_game/number_game.js
@@ -1,13 +1,85 @@
-/*
-Let's go back to our game from yesterday where we displayed the number
-of guesses. The goal is now to add some persistence to our game so that
-it remembers the best score and the total number of games.
+const readline = require("readline");
+const fs = require("fs");
 
-Be sure to update your original interface to display these numbers to
-the player each time the game starts and ends.
+let score = {
+  nbTries: 0,
+  bestScore: 0
+};
 
-It should look like this:
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-> node number-game.js
-Welcome to the number game You played X games and your best score is: Y
-*/
+function getNumberToFind(minNumber, maxNumber) {
+  return Math.floor(Math.random() * Math.floor(maxNumber)+1);
+}
+
+function analyzeResponse(pnumberToFind, pminNumber, pmaxNumber, presponse) {
+  let result = "error";
+  if (isNaN(presponse)) {
+    result = "This was not a number\n";
+  } else {
+    const currentNumber = parseInt(presponse, 10);
+    if (currentNumber < pminNumber || currentNumber > pmaxNumber) {
+      result = "The number is between 1 and 100\n";
+    } else if (currentNumber === pnumberToFind) {
+      result = "You won!";
+    } else if (currentNumber < pnumberToFind) {
+      result = "Too low\n";
+    } else if (currentNumber > pnumberToFind) {
+      result = "Too high\n";
+    }
+  }
+  return result;
+}
+
+function askQuestion(currentQuestion, pmysteryNumber, pminNumber, pmaxNumber, pnbTest) {
+  reader.question(currentQuestion, response => {
+    const currentAnalyze = analyzeResponse(pmysteryNumber, pminNumber, pmaxNumber, response);
+    if (currentAnalyze === "You won!") {
+      console.log(currentAnalyze + " / " + pnbTest + " times");
+      reader.close();
+      saveScore(pnbTest);
+    } else {
+      askQuestion(currentAnalyze, pmysteryNumber, pminNumber, pmaxNumber, pnbTest+1);
+    }
+  });
+}
+
+function getScore(callback) {
+  fs.readFile("./data.json", (error, data) => {
+    if (error) {
+      console.warn(error);
+      return;
+    }
+    score = JSON.parse(data);
+    console.log("Best score = ", score);
+    callback();
+  });
+}
+
+function saveScore(newScore) {
+  score.nbTries++;
+  if (score.bestScore === 0 || newScore < score.bestScore) {
+    score.bestScore = newScore;
+  }
+  fs.writeFile("./data.json", JSON.stringify(score), (error) => {
+    if (error) {
+      console.warn(error);
+    } else {
+      console.log("The score was saved!");
+    }
+  });
+}
+
+const minNumber = 1;
+const maxNumber = 100;
+const nbTest = 1;
+
+const mysteryNumber = getNumberToFind(minNumber, maxNumber);
+console.log("Welcome to the number game (" + mysteryNumber + ")");
+
+getScore(function() {
+   askQuestion("un nombre ? ", mysteryNumber, minNumber, maxNumber, nbTest);
+});
