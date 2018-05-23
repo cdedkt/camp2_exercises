371d083966768f0a4ed6dd1059e78992fe77881b
cdedkt <christophe.delattre@decathlon.com>
2018-05-23 13:18:51 +0200

    reactjs

--- 06_react_redux/01_es6_fundamentals/01_aquarium/01_aquarium.js
+++ 06_react_redux/01_es6_fundamentals/01_aquarium/01_aquarium.js
@@ -1,6 +1,105 @@
-// Insert code here
+let globalId = 1000;
 
+class Aquarium {
+  constructor(fishes, algaes) {
+    this.fishes = fishes;
+    this.algaes = algaes;
+  }
 
+  addFish(fish) {
+    this.fishes.push(fish);
+  }
+
+  addAlgae(algae) {
+    this.algaes.push(algae);
+  }
+
+  eatOneAlgaeBy(fish) {
+    if (this.algaes.length > 0) {
+      const algeaEaten = this.algaes.splice(0, 1)[0];
+      console.log("algea ", algeaEaten.getInformation(), " eaten by ", fish.getInformation());
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  eatOneFishBy(fish) {
+    if (this.fishes.length === 1) {
+      // last fish
+      return false;
+    } else {
+      let fishEaten = "";
+      if (this.fishes[0].id !== fish.id) {
+        fishEaten = this.fishes.splice(0, 1)[0];
+      } else {
+        //un poisson ne se mange pas lui meme, on mange le suivant
+        fishEaten = this.fishes.splice(1, 1)[0];
+      }
+      console.log("fish ", fishEaten.getInformation(), " eaten by ", fish.getInformation());
+    }
+  }
+
+  passTime() {
+    console.log("\n>>> pastTime BEFORE");
+    console.log("Number of algaes : ", this.algaes.length);
+    this.fishes.forEach(fish => {
+      console.log("fish", fish.getInformation());
+    });
+
+    this.fishes.forEach(fish => {
+     fish.eat(this);
+    });
+
+    // this.fishes.forEach(function(fish) {
+    //   fish.eat(this);
+    // });
+
+    console.log(">>> pastTime AFTER");
+    console.log("Number of algaes : ", this.algaes.length);
+    this.fishes.forEach(fish => {
+      console.log("fish", fish.getInformation());
+    });
+
+  }
+
+  endTurn() {
+    while (this.fishes.length > 1) {
+      this.passTime();
+    }
+  }
+}
+
+class Fish {
+  constructor(name, sex, isVegan) {
+    this.name = name;
+    this.sex = sex;
+    this.isVegan = isVegan;
+    this.id = ++globalId;
+  }
+
+  getInformation() {
+    return "-" + this.name + "- " + this.sex + " " + (this.isVegan ? "vegan" : "carnivor") + " id:" + this.id;
+  }
+
+  eat(aquarium) {
+    if (this.isVegan) {
+      aquarium.eatOneAlgaeBy(this);
+    } else {
+      aquarium.eatOneFishBy(this)
+    }
+  }
+}
+
+class Algae {
+  constructor(size) {
+    this.size = size;
+  }
+
+  getInformation() {
+    return "-" + this.size + "-";
+  }
+}
 
 module.exports = {
   aquarium: Aquarium,
--- 06_react_redux/01_es6_fundamentals/02_spread_destructuring/02_spread_destructuring.js
+++ 06_react_redux/01_es6_fundamentals/02_spread_destructuring/02_spread_destructuring.js
@@ -1,36 +1,60 @@
 function go(options) {
   let {
     speed = 4,
-    enable: { hyperdrive = false, frobnifier = true}
+    enable: { hyperdrive = false, frobnifier = true} = {}
   } = options;
 
-  console.log("speed=", speed, "hyperdrive:", hyperdrive, "frobnifier:", frobnifier);
+  console.log("speed=", speed, "hyperdrive=", hyperdrive, "frobnifier=", frobnifier);
 }
 
+go({ speed: 5, enable: {hyperdrive: "tutu", frobnifier:"toto"} });
 go({ speed: 5 });
 
-function lastIndexOf(arr, elt, start) {
+//function lastIndexOf(arr, elt, start) {
+function lastIndexOf(arr, elt, start = arr.length-1) {
+
+  console.log("start=", start);
+
   for (let i = start - 1; i >= 0; i--) {
     if (arr[i] === elt) {
       return i;
     }
   }
-
   return -1;
 }
 
 console.log(lastIndexOf([1, 2, 1, 2], 2));
+console.log(lastIndexOf([1, 2, 1, 2], 2, 3));
+console.log(lastIndexOf([1, 2, 1, 2], 2, 2));
+console.log(lastIndexOf([1, 2, 1, 2], 1));
+console.log(lastIndexOf([1, 2, 1, 2], 1, 2));
+
 
 function replace(array, from, to, elements) {
   array.splice.apply(array, [from, to - from].concat(elements));
 }
 
+function replace2(array, from, to, elements) {
+    array.splice(from, to-from, ...elements);
+}
+
 let testArray = [1, 2, 100, 100, 6];
 replace(testArray, 2, 4, [3, 4, 5]);
-console.log(testArray);
+console.log("replace:", testArray);
+
+testArray = [1, 2, 100, 100, 6];
+replace2(testArray, 2, 4, [3, 4, 5]);
+console.log("replace2:", testArray);
+
 
 function copyReplace(array, from, to, elements) {
   return array.slice(0, from).concat(elements).concat(array.slice(to));
 }
 
-console.log(copyReplace([1, 2, 100, 200, 6], 2, 4, [3, 4, 5]));
+function copyReplace2(array, from, to, elements) {
+  return array.splice(from, to-from, ...elements);
+}
+
+
+console.log("copyReplace:", copyReplace([1, 2, 100, 200, 6], 2, 4, [3, 4, 5]));
+console.log("copyReplace2:", copyReplace2([1, 2, 100, 200, 6], 2, 4, [3, 4, 5]));
