97fafb849eaa28665ba46db12313733d97a0ff92
cdedkt <christophe.delattre@decathlon.com>
2018-05-24 15:34:22 +0200

    mm

--- 06_react_redux/04_lifecycle_method/01_fetch_data/src/App.js
+++ 06_react_redux/04_lifecycle_method/01_fetch_data/src/App.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class DogApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: "",
+    };
+  }
+
+  nextImage() {
+    fetch(
+      `https://random.dog/woof.json`,
+      {method: "GET"}
+    )
+      .then((response) => response.json())
+      .then((dog) => {
+        console.log("dog.url=",dog.url);
+        this.setState({
+          url: dog.url,
+        });
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  }
+
   render() {
+    console.log("render");
     return (
       <div className="App">
-
+        <br/>
+        <img src={this.state.url}/>
+        <br/>
+        <br/>
+        <button onClick={() => this.nextImage()}>next</button>
       </div>
     );
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.nextImage();
   }
 }
 
--- 06_react_redux/04_lifecycle_method/02_transform_data/src/App.js
+++ 06_react_redux/04_lifecycle_method/02_transform_data/src/App.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import _ from "underscore"
+
+function TableHeader() {
+  return (
+    <tr>
+      <th scope="col">what has been paid</th>
+      <th scope="col">by whom?</th>
+    </tr>
+  )
+}
+
+class TableRow extends Component {
+  render() {
+    return (
+      <tr>
+        <td scope="row">{this.props.what}</td>
+        <td>{this.props.by}</td>
+      </tr>
+    )
+  }
+}
 
 // App has `expenses` and `users` as props
 class App extends Component {
   render() {
+
     return (
       <div className="App">
+        <table className="table table-striped mt-4">
+          <thead>
+            <TableHeader/>
+          </thead>
+          <tbody>
+          {this.props.expenses.map((expense, index) => {
 
+            const userFound = _.findWhere(this.props.users, {id: expense.who});
+            let username = "";
+            console.log("userFound=", userFound);
+            if (userFound !== undefined) {
+              username = userFound.name;
+            }
+
+            return (<TableRow key={index}
+              what={expense.what}
+              by={username}
+            />)
+            }
+          )}
+          </tbody>
+        </table>
       </div>
     );
   }
